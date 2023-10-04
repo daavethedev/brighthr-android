302d2821bfda6a3cec1a044ae61205120c472fec
tanion <tanion.baboutila@brighthr.com>
2023-10-04 10:37:16 +0100

    chore: organise the imports

--- app/src/main/java/space/stanton/technicaltest/brightones/features/post/ui/PostScreen.kt
+++ app/src/main/java/space/stanton/technicaltest/brightones/features/post/ui/PostScreen.kt
@@ -6,17 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
