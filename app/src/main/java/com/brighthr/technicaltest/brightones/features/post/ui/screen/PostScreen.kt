10db55aa3e38bffbf5605b830d610013280bd9c8
daavethedev <umair_mnz105@yahoo.com>
2025-07-04 12:03:18 +0100

    - PostScreen.kt updated to integrate DaggerHilt with ViewModel. -

--- app/src/main/java/com/brighthr/technicaltest/brightones/features/post/ui/screen/PostScreen.kt
+++ app/src/main/java/com/brighthr/technicaltest/brightones/features/post/ui/screen/PostScreen.kt
@@ -1,24 +1,12 @@
 package com.brighthr.technicaltest.brightones.features.post.ui.screen
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.brighthr.technicaltest.brightones.features.post.model.Post
+import com.brighthr.technicaltest.brightones.features.post.ui.screen.component.PostListView
 import com.brighthr.technicaltest.brightones.features.post.viewmodel.PostViewModel
 
 @Composable
@@ -29,52 +17,10 @@ fun PostScreen(
 
     val posts by viewModel.posts.collectAsState()
 
-    PostView(
-        modifier = modifier,
-        posts = posts,
-        postViewModel = viewModel,
-        loadPost = viewModel::loadPost
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PostView(
-    modifier: Modifier, posts: List<Post>, postViewModel: PostViewModel, loadPost: () -> Unit
-) {
-
-    loadPost()
-
-    Scaffold(
-        modifier = modifier, topBar = {
-            TopAppBar(title = { Text(text = "Post List") })
-        }) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-        ) {
-            posts.filter { it.body != null }.sortedBy { it.id }.forEach {
-                PostView(post = it, postViewModel = postViewModel)
-                HorizontalDivider(modifier = Modifier.padding(top = 8.dp, bottom = 16.dp))
-            }
-        }
+    LaunchedEffect(Unit) {
+        viewModel.loadPost()
     }
-}
 
-@Composable
-fun PostView(post: Post, postViewModel: PostViewModel) {
-    Column(verticalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier
-        .clickable {
-            postViewModel.navigateToDetails()
-        }
-        .fillMaxWidth()
-        .padding(horizontal = 16.dp)) {
-        Text(
-            text = post.title, style = MaterialTheme.typography.titleSmall
-        )
-        post.body?.let {
-            Text(text = it)
-        }
-    }
+    PostListView(
+        modifier = modifier, posts = posts, onPostClick = { viewModel.navigateToDetails() })
 }
